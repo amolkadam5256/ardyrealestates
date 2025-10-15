0ce3ea3715ac58af7595bead6ae4c622b5e7a6fd
amolkadam5256 <amolkadam1274@gmail.com>
2025-10-15 15:21:37 +0530

    first commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutHeroSection from '../components/AboutSection/AboutHeroSection'
+import Testimonials from '../components/HomeSection/Testimonials'
 const About = () => {
   return (
     <>
       <main>
         <AboutHeroSection/>
+        <Testimonials/>
       </main>
     </>
   )
